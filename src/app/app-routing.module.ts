138a0d6fd0ec4d23b16a7d48fe113314f19bed3f
marcos <marcoscorreats@gmail.com>
2022-03-04 19:02:47 -0300

    feat(Comments) Comment some

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   {
     path: '',
+    // Example for LazyLoading mode, thats help to better performance of Angular. Its one of Best Pratices Angular
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
   },
-  {
-    path:'',
-    redirectTo:'login',
-    pathMatch:'full'
-  },
+  // {
+  //   path:'',
+  //   redirectTo:'login',
+  //   pathMatch:'full'
+  // },
   {
     path: 'login',
     loadChildren: () => import('./account/account.module').then(m => m.AccountModule)
@@ -25,6 +27,9 @@ const routes: Routes = [
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
     canActivate:[AuthGuard]
   },
+  /**
+   * Any Broken Routes redirect to login component
+   */
   {
     path:'**',
     redirectTo:'login'
--- src/app/authentication/guard/auth.guard.ts
+++ src/app/authentication/guard/auth.guard.ts
@@ -7,7 +7,20 @@ import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from
 export class AuthGuard implements CanActivate {
 
   constructor(private router: Router){}
-
+  /**
+   * @method canActivate
+   * @param  {ActivatedRouteSnapshot} route
+   * @param  {RouterStateSnapshot} state
+   * @returns boolean
+   * @description 
+   * 
+   * Return true or false to exists object, faking a token validation.
+   * 
+   * That is used with Router configs to block acces for the Admin panel,
+   * 
+   * If the actual Session hasn't the token, all routes with canActivate pipe automaticaly redirects to Login Component
+   * 
+   */
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot): boolean {
     const token = window.localStorage.getItem('book_comet_token');
     if(!token) this.router.navigate(['login'])
